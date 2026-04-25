6743c204fbff4e885e45258881437b6c399ddca6
Giorgos <giorgosslim@gmail.com>
2026-04-25 20:58:42 +0300

    navBar done theme is ready

--- menu-effects.js
+++ menu-effects.js
@@ -1,4 +1,27 @@
-let menu = document.querySelector('.menu__header')
+/* theme switch start*/
+let darkMode = localStorage.getItem('dark-mode');
+const themeSwitch = document.querySelector('#theme-button');
 
-function changeTheme() {
+function enableDarkMode() {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('dark-mode', 'active');
 }
+
+function disableDarkMode() {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('dark-mode', null);
+}
+
+if (darkMode === "active") {
+    enableDarkMode();
+}
+
+themeSwitch.addEventListener('click', () => {
+    darkMode = localStorage.getItem('dark-mode');
+    if (darkMode !== 'active') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+})
+/*theme switch end*/
